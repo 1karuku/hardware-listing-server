8b1a70708e5bbe23d794efa0b6aa04a1fa010f7c
karuku <robert.n.karuku@gmail.com>
2019-07-30 17:01:15 +0300

    adjusting my database connection

--- index.js
+++ index.js
@@ -4,20 +4,22 @@ const mysql = require("mysql");
   const app = express();
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
  });
 
- app.get("/api/materials", (req, res) => {
-     pool.query("SELECT material_id, material_name FROM materials",
+ app.get("/api/hardware/:id", (req, res) => {
+     pool.query(
+         "SELECT material_id, material_name,material_prize FROM materials WHERE id = ?",
+         [req.params.id],
       (error, rows) => {
          if (error) {
              return res.status(500).json({ error });
          }
 
          res.json(rows);
-     });
- });
-
-  app.listen(9000, () => console.log("App listening on port 9000"));
+        });
+    });
+  app.listen(9000, () =>  console.log("App listening on port 9000"));
